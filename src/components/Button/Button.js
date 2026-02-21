72184e64de9acee62716d1812e4935f099df4d0b
Kamil Bieniek <bienius.234@gmail.com>
2026-02-21 15:18:44 +0100

    Add all functions and create empty files for components

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,18 @@ import styles from './Button.module.scss';
 import clsx from 'clsx';
 
 const Button = (props) => {
-    return (<button className={clsx(styles.button, props.className)}>{props.children}</button>);
+
+    const addToCart = (e) =>{
+        e.preventDefault();
+        console.log('Summary');
+        console.log('==========');
+        console.log('Name:',props.name,);
+        console.log('Price:',props.price,);
+        console.log('Size:',props.size,);
+        console.log('Color:',props.color,);
+    };
+
+    return (<button onClick ={addToCart} className={clsx(styles.button, props.className)}>{props.children}</button>);
 };
 
 export default Button;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,46 +1,73 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Product = props => {
+  // [DONE] zamienić stałe dane na parametry 
+  // [DONE] uzyc metody map 
+  //wydzielic divy z opcjami koloru czy rozmiaru wydzielić do komponentów
+  //zmienic pomysł nadawania kolorów dla opcji colors - elemt li ma klase dedykowaną z kolorem
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.sizes[0]['name']);
+
+  const getPrice = () =>{
+    const currentSizeData = props.sizes.find(size => size.name === currentSize);
+    const additionalPrice = currentSizeData?.additionalPrice;
+    const totalPrice = props.basePrice + additionalPrice;
+    return totalPrice;
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={props.title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map(size =><li key={size.name}>
+                <button 
+                  type="button" 
+                  className={clsx(currentSize === size['name'] && styles.active)} 
+                  onClick={() => setCurrentSize(size['name'])} >
+                  {size['name']}
+                </button></li>)}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+               {props.colors.map(color =><li key={color}>
+                <button 
+                  type="button" 
+                  className={clsx(styles[`color${color.charAt(0).toUpperCase() + color.slice(1)}`], currentColor === color && styles.active)} 
+                  onClick={() => setCurrentColor(color)}/>
+                </li>)}
             </ul>
           </div>
-          <Button className={styles.button}>
+          <Button className={styles.button} name={props.name} size={currentSize} price={getPrice()} color={currentColor}>
             <span className="fa fa-shopping-cart" />
           </Button>
         </form>
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  size:PropTypes.string,
+  color:PropTypes.string
 };
 
 export default Product;
